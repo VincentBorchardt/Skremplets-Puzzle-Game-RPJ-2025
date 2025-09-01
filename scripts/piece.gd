8c859d905e60defa619c910ace1b7933254105ff
VincentBorchardt <vborchardt123@gmail.com>
2025-08-31 22:18:36 -0500

    Copy over Piece
    The Piece Resource seems like it's in a good starting place, so I just copied it over (with a couple TODOs in it to add stuff)--I also should move more stuff from the various other classes to it, but that comes later

--- scripts/piece.gd
+++ scripts/piece.gd
@@ -1,1 +1,50 @@
 class_name Piece extends Resource
+
+@export var owner: Player
+@export var root_point_location: Vector2i
+@export var secondary_points: Array[Vector2i]
+@export var image: Texture2D
+@export var type: Type
+@export var translation: Vector2
+# currently in radians (so 90 degrees = TAU/4), 
+@export var rotation: float
+
+#TODO These need to be updated for the final project
+enum Player {PLAYER_1, PLAYER_2, UNOWNED}
+enum Type {RED, BLUE, YELLOW, WILD, NONE}
+
+func is_touching(piece2):
+	#TODO This doesn't account for WILD yet
+	if self == piece2 or self.type != piece2.type:
+		return false
+	else:
+		for point1 in self.secondary_points:
+			for point2 in piece2.secondary_points:
+				var x_dist = abs(point1.x - point2.x)
+				var y_dist = abs(point1.y - point2.y)
+				if x_dist <= 1 and y_dist <= 1 and not (x_dist == 1 and y_dist == 1):
+					return true
+	return false
+
+# This function is going to be hard--right now I'm not going to check for off-the-grid
+# and that will cause false positives, but they can be caught elsewhere
+func get_adjacent_points():
+	var adjacent_points = []
+	for starting_point in self.secondary_points:
+		var possible_points = [starting_point + Vector2i(1, 0), starting_point - Vector2i(1, 0), 
+		starting_point + Vector2i(0, 1), starting_point - Vector2i(0, 1)]
+		for candidate in possible_points:
+			if not (adjacent_points.has(candidate) or self.secondary_points.has(candidate)):
+				adjacent_points.append(candidate)
+	return adjacent_points
+
+func points_on_grid():
+	#TODO This hardcodes the grid size, change that in the final code
+	for point in self.secondary_points:
+		if (point.x < 0 or point.y < 0 or point.x > 4 or point.y > 4):
+			return false
+		else:
+			return true
+
+func _to_string() -> String:
+	return "Piece at " + str(root_point_location) + ", " + str(secondary_points)
