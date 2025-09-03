bbbdcfd5d79e348dffb81027f1c85183ece8a062
VincentBorchardt <vborchardt123@gmail.com>
2025-09-02 23:55:18 -0500

    PieceList functions moved into their own class
    After a false alarm, stuff like checking overlaps and if pieces touch are now in their own class rather than being put on top of the visual grid. The grids are still linked, but that's the current major bug.

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -11,7 +11,7 @@ var grid_group = "Grid Spaces " + str(grid_owner)
 
 #TODO This should be its own class with helper methods (presumably that's a resource?)
 # Stuff like "get all the red pieces" or "get all the spaces next to a given piece"
-var piece_dict = {}
+var piece_list = PieceList.new()
 
 #TODO This needs to be updated for the final project and not duplicated everywhere
 enum Player {PLAYER_1, PLAYER_2, UNOWNED}
@@ -31,11 +31,8 @@ func _on_grid_space_add_new_piece(new_piece, new_location, player):
 	if player != grid_owner:
 		print("Not Player 1's Grid")
 		return false
-	for piece in piece_dict:
-		for point in new_piece.secondary_points:
-			if piece_dict[piece].has(point + new_location):
-				print(str(new_location) + " is already covered")
-				return false
+	if piece_list.has_overlaps(new_piece, new_location):
+		return false
 	var piece_points = []
 	for point in new_piece.secondary_points:
 		var new_point = point + new_location
@@ -50,8 +47,8 @@ func _on_grid_space_add_new_piece(new_piece, new_location, player):
 	for i in range(new_piece.secondary_points.size()):
 		var point = new_piece.secondary_points[i]
 		new_piece.secondary_points[i] = point + new_location
-	piece_dict[new_piece] = piece_points
-	print(get_tree().get_nodes_in_group(grid_group).size())
+	piece_list.add(new_piece, piece_points)
+	#print(get_tree().get_nodes_in_group(grid_group).size())
 	for node in get_tree().get_nodes_in_group(grid_group):
 		node._on_add_new_piece(new_piece, new_location)
 	Inventory.current_piece = null
@@ -59,11 +56,10 @@ func _on_grid_space_add_new_piece(new_piece, new_location, player):
 	return true
 
 func try_to_clear_pieces():
-	var pieces_to_clear = check_pieces_touching()
+	var pieces_to_clear = piece_list.get_touching_pieces()
 	if not pieces_to_clear.is_empty():
 		print("removing pieces")
-		for piece in pieces_to_clear:
-			piece_dict.erase(piece)
+		piece_list.remove_pieces(pieces_to_clear)
 		for node in get_tree().get_nodes_in_group(grid_group):
 			node.remove_pieces(pieces_to_clear)
 		try_to_clear_pieces()
@@ -71,37 +67,3 @@ func try_to_clear_pieces():
 func point_is_off_grid(point):
 	# TODO This should be removed and point to the piece off_grid function
 	return point.x < 0 or point.y < 0 or point.x >= grid_x or point.y >= grid_y
-
-
-func check_pieces_touching():
-	print("in check_pieces_touching")
-	var placed_pieces = piece_dict.keys()
-	var num_pieces = placed_pieces.size()
-	for i in range(num_pieces):
-		var piece_1 = placed_pieces[i]
-		if piece_1.type != Type.WILD:
-			for j in range(num_pieces):
-				var piece_2 = placed_pieces[j]
-				if piece_1.is_touching(piece_2):
-					var initial_pair = [piece_1, piece_2]
-					var pieces_to_check = extended_piece_touching_check(initial_pair)
-					if pieces_to_check.size() >= 3:
-						return pieces_to_check
-	return []
-
-func extended_piece_touching_check(initial_pair):
-	print("in extended_piece_touching_check")
-	var pieces_to_check = initial_pair.duplicate()
-	var placed_pieces = piece_dict.keys()
-	var num_pieces = placed_pieces.size()
-	var found_new_piece = true
-	while(found_new_piece):
-		found_new_piece = false
-		for i in range(num_pieces):
-			var new_piece = placed_pieces[i]
-			if not pieces_to_check.has(new_piece):
-				for piece in pieces_to_check:
-					if piece.is_touching(new_piece):
-						pieces_to_check.append(new_piece)
-						found_new_piece = true
-	return pieces_to_check
--- scripts/piece_list.gd
+++ scripts/piece_list.gd
@@ -4,5 +4,53 @@ class_name PieceList extends Resource
 # one of the points of this class is to hide that implementation detail
 var list = {}
 
+#TODO This needs to be updated for the final project and not duplicated everywhere
+enum Player {PLAYER_1, PLAYER_2, UNOWNED}
+enum Type {RED, BLUE, YELLOW, WILD, NONE}
+
 func add(piece, points):
 	list[piece] = points
+
+func remove_pieces(pieces_to_clear):
+	for piece in pieces_to_clear:
+		list.erase(piece)
+
+func has_overlaps(new_piece, new_location):
+	for piece in list:
+		for point in new_piece.secondary_points:
+			if list[piece].has(point + new_location):
+				print(str(new_location) + " is already covered")
+				return true
+
+func get_touching_pieces():
+	print("in check_pieces_touching")
+	var placed_pieces = list.keys()
+	var num_pieces = placed_pieces.size()
+	for i in range(num_pieces):
+		var piece_1 = placed_pieces[i]
+		if piece_1.type != Type.WILD:
+			for j in range(num_pieces):
+				var piece_2 = placed_pieces[j]
+				if piece_1.is_touching(piece_2):
+					var initial_pair = [piece_1, piece_2]
+					var pieces_to_check = extended_piece_touching_check(initial_pair)
+					if pieces_to_check.size() >= 3:
+						return pieces_to_check
+	return []
+
+func extended_piece_touching_check(initial_pair):
+	print("in extended_piece_touching_check")
+	var pieces_to_check = initial_pair.duplicate()
+	var placed_pieces = list.keys()
+	var num_pieces = placed_pieces.size()
+	var found_new_piece = true
+	while(found_new_piece):
+		found_new_piece = false
+		for i in range(num_pieces):
+			var new_piece = placed_pieces[i]
+			if not pieces_to_check.has(new_piece):
+				for piece in pieces_to_check:
+					if piece.is_touching(new_piece):
+						pieces_to_check.append(new_piece)
+						found_new_piece = true
+	return pieces_to_check
