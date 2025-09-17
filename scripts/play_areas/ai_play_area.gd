b5c6486ba985acbdee407622f6db86f49253afdb
VincentBorchardt <vborchardt123@gmail.com>
2025-09-16 23:54:25 -0500

    New pieces, new test_scene
    There are now two pieces of each (non-purple) type, and I have a new scene to ensure the genericized play_scenes work (and it proves that different AIs can lead to different experiences). I also added some new assets, even if only the unique power-up bar fills are implemented so far.

--- scripts/play_areas/ai_play_area.gd
+++ scripts/play_areas/ai_play_area.gd
@@ -6,15 +6,49 @@ func get_preferred_piece(neutral_piece_list):
 	match character.ai_picking_pattern:
 		Inventory.AIPickingPattern.RANDOM:
 			return pick_random_piece(neutral_piece_list)
+		Inventory.AIPickingPattern.MOST:
+			return pick_most_blocks(neutral_piece_list)
+		Inventory.AIPickingPattern.BIGGEST:
+			return pick_biggest_piece(neutral_piece_list)
 
 func pick_random_piece(neutral_piece_list):
 	var index = randi() % neutral_piece_list.size()
 	var piece = neutral_piece_list.get_piece_at_index(index)
 	return piece
 
+func pick_most_blocks(neutral_piece_list):
+	var num_blocks = 0
+	var picked_piece
+	for piece in neutral_piece_list.get_pieces():
+		var piece_blocks = piece.secondary_points.size()
+		if piece_blocks > num_blocks:
+			num_blocks = piece_blocks
+			picked_piece = piece
+	if picked_piece:
+		return picked_piece
+	else:
+		print("pick_most_blocks failed to get a piece")
+		return pick_random_piece(neutral_piece_list)
+
+func pick_biggest_piece(neutral_piece_list):
+	var picked_dimensions = 0
+	var picked_piece
+	for piece in neutral_piece_list.get_pieces():
+		var piece_dimensions = piece.x_length + piece.y_length
+		if piece_dimensions > picked_dimensions:
+			picked_dimensions = piece_dimensions
+			picked_piece = piece
+		if picked_piece:
+			return picked_piece
+		else:
+			print("pick_biggest_piece failed to get a piece")
+			return pick_random_piece(neutral_piece_list)
+
 func start_ai_place():
 	var piece_list = $PlayGrid.piece_list
 	match character.ai_placement_pattern:
+		# TODO Grymmt is currently random-matching instead of anti-garbage,
+		# since I need to implement the latter
 		Inventory.AIPlacementPattern.RANDOM_MATCHING:
 			if piece_list.is_empty() or piece_list.has_only_special():
 				place_fully_random_piece(0)
@@ -23,11 +57,9 @@ func start_ai_place():
 
 func place_fully_random_piece(num_of_failures):
 	# TODO turn this into a while loop instead of recursion?
-	#print("placing fully random piece")
 	var rand_space = randi() % ($PlayGrid.grid_x * $PlayGrid.grid_y)
 	var rand_coord = Vector2i(rand_space % $PlayGrid.grid_x, floori(rand_space / $PlayGrid.grid_y))
 	var rand_rotation = randi() % 4
-	#print("attempting to place piece at " + str(rand_coord) + " with " + str(rand_rotation) + " rotations")
 	rotate_current_piece(rand_rotation * (TAU / 4))
 	if $PlayGrid.add_new_piece(current_piece, rand_coord, grid_owner):
 		print("successful adding piece")
@@ -48,8 +80,6 @@ func place_fully_random_piece(num_of_failures):
 
 func place_selective_piece(piece_dict):
 	for piece in piece_dict.get_pieces():
-		#print(piece)
-		#print(current_piece)
 		if piece.type == current_piece.type:
 			if try_to_place_connecting_piece(piece, 0):
 				print("successful placing selective piece")
